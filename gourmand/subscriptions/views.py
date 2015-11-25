66b61250dab44da6a7503905b3adbe33943c35c5
Aaron Kurtz <a.kurtz@hardsun.net>
2015-11-25 01:17:48 -0800

    Include unread, all article count for subscriptions

--- gourmand/subscriptions/views.py
+++ gourmand/subscriptions/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Count
 from django.views.generic import TemplateView, ListView
 
 from braces.views import LoginRequiredMixin
@@ -13,4 +14,10 @@ class Reader(LoginRequiredMixin, ListView):
     template_name = "reader.html"
 
     def get_queryset(self):
-        return Subscription.objects.filter(owner=self.request.user).select_related('feed')
+        subs = Subscription.objects.filter(owner=self.request.user).select_related('feed')
+        # Extra modifier is required to annotate unread as well as articles
+        # Conditional Count in 1.8 works, but breaks if combined with another Count, despite using distinct=True
+        subs = subs.annotate(articles=Count('feed__article')).\
+            extra(select={'unread': 'SELECT COUNT(*) FROM subscriptions_personalarticle WHERE ' +
+                          'subscriptions_subscription.id = subscriptions_personalarticle.sub_id AND active IS TRUE'})
+        return subs
