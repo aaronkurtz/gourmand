26f80196cecf187020c1d540fa50dbd8962a976b
Aaron Kurtz <a.kurtz@hardsun.net>
2015-11-29 01:30:41 -0800

    Add BS3 theme in new static_files directory

--- gourmand/config/settings.py
+++ gourmand/config/settings.py
@@ -101,5 +101,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    BASE_DIR('static_files'),
+)
 STATIC_ROOT = BASE_DIR('static')
 WHITENOISE_ROOT = BASE_DIR('root_files')
