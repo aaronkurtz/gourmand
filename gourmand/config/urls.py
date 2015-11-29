fc6323a1a9cc6ab44d131511fdc0b38484738d05
Aaron Kurtz <a.kurtz@hardsun.net>
2015-11-29 02:37:16 -0800

    Add login url and template

--- gourmand/config/urls.py
+++ gourmand/config/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 import feeds.urls
 import subscriptions.urls
 
 urlpatterns = [
     url(r'', include(subscriptions.urls)),
+    url(r'^accounts/login/$', auth_views.login, name='login'),
+    url(r'^accounts/logout/$', auth_views.logout, {'next_page': 'front'}, name='logout'),
     url(r'^feeds/', include(feeds.urls)),
     url(r'^admin/', include(admin.site.urls)),
 ]
