4d08e03b813289b3bc7f38308ec76bc17c71f40c
Aaron Kurtz <a.kurtz@hardsun.net>
2015-12-09 01:01:32 -0800

    Improve async OPML import

--- gourmand/subscriptions/views.py
+++ gourmand/subscriptions/views.py
@@ -1,4 +1,6 @@
 from collections import Counter
+import time
+
 from django.contrib import messages
 from django.core.exceptions import PermissionDenied, ValidationError
 from django.core.urlresolvers import reverse_lazy
@@ -10,13 +12,19 @@ from django.views.generic import TemplateView, ListView, FormView, View, Redirec
 
 from async_messages import messages as async_messages
 from braces.views import LoginRequiredMixin, UserFormKwargsMixin
-from django_q.tasks import async, async_iter, result
+from django_q.humanhash import uuid
+from django_q.tasks import async, result_group, count_group, delete_group
+from django_q.conf import logger
 import feedparser
+import requests
 
 from feeds.models import Feed
 from .forms import NewSubForm, ImportOPMLForm
 from .models import Subscription, PersonalArticle
 from .utils import create_opml
+
+FEED_GET_TIMEOUT = 10
+IMPORT_WAIT = 2 * 60
 
 
 class FrontPage(TemplateView):
@@ -93,7 +101,7 @@ class ImportOPML(LoginRequiredMixin, UserFormKwargsMixin, FormView):
         num_existing = len(existing_urls)
         num_fresh = len(fresh_urls)
         if fresh_urls:
-            async(import_urls, self.request.user, fresh_urls)
+            async(import_urls, self.request.user, fresh_urls, group='opml_import')
         if num_fresh:
             messages.success(self.request, "You imported {sub} feed{s} - processing now.".format(sub=num_fresh, s=pluralize(num_fresh)))
         if num_existing:
@@ -103,30 +111,55 @@ class ImportOPML(LoginRequiredMixin, UserFormKwargsMixin, FormView):
 
 
 def import_urls(user, fresh_urls):
-    result_id = async_iter(subscribe_to_imported_url, [(user, url) for url in fresh_urls], timeout=30)
-    import_results = Counter(result(result_id, -1))
-    print("Results", import_results)
-    added = import_results['added']
-    existed = import_results['existed']
-    errors = import_results['errors']
-    async_messages.success(user, "Import complete - you subscribed to {sub} feed{s}.".format(sub=added, s=pluralize(added)))
-    if existed:
+    group = uuid()
+    size = len(fresh_urls)
+    for url in fresh_urls:
+        async(subscribe_to_imported_url, user, url, group=group)
+    start = time.time()
+    while True:
+        if (time.time() - start) > IMPORT_WAIT:
+            break
+        if count_group(group) == size:
+            break
+        time.sleep(1)
+    import_results = Counter(result_group(group))
+    pretty_results = ', '.join("{}: {}".format(*x) for x in import_results.items())
+    num_added = import_results['added']
+    num_existed = import_results['existed']
+    num_errors = import_results['error']
+    if num_added:
+        async_messages.success(user, "Import complete - you subscribed to {sub} feed{s}.".format(sub=num_added, s=pluralize(num_added)))
+    else:
+        async_messages.info(user, "Import complete - no new subscriptions were added.")
+    if num_existed:
         async_messages.info(user,
-                            "You were already subscribed to {sub_exists} imported feed{s}.".format(sub_exists=existed, s=pluralize(existed)))
-    if errors:
-        async_messages.error(user, "There was an error subscribing to {errors} imported feed{s}.".format(errors=errors, s=pluralize(errors)))
+                            "You were already subscribed to {sub_exists} imported feed{s}.".format(sub_exists=num_existed, s=pluralize(num_existed)))
+    if num_errors:
+        async_messages.error(user, "There was an error subscribing to {errors} imported feed{s}.".format(errors=num_errors, s=pluralize(num_errors)))
+    logger.info('User %(user)s OPML import complete - %(results)s' % {'user': user, 'results': pretty_results})
+    delete_group(group, tasks=True)
+    return pretty_results
 
 
 def subscribe_to_imported_url(user, url):
     try:
-        try:
+        if Feed.objects.filter(href=url).exists():
             feed = Feed.objects.get(href=url)
-        except Feed.DoesNotExist:
-            fp = feedparser.parse(url)
-            feed = Feed.objects.create_from_feed(fp)
-            feed.full_clean()
-            feed.save()
-            feed.update(fp)
+        else:
+            try:
+                r = requests.get(url, timeout=FEED_GET_TIMEOUT)
+            except requests.exceptions.RequestException:
+                return('error')
+            new_url = r.url
+
+            if url != new_url and Feed.objects.filter(href=new_url).exists():
+                feed = Feed.objects.get(href=r.url)
+            else:
+                fp = feedparser.parse(r.content)
+                feed = Feed.objects.create_from_feed(parsed_feed=fp, href=new_url)
+                feed.full_clean()
+                feed.save()
+                feed.update(parsed_feed=fp, href=new_url)
         sub, created = Subscription.objects.get_or_create(owner=user, feed=feed)
         sub.populate()
         if created:
