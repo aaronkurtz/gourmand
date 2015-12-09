4f8a76f2de294fb48fbf7cb1a8aa829accde4564
Aaron Kurtz <a.kurtz@hardsun.net>
2015-12-09 06:38:04 -0800

    Add summary to update_feeds

--- gourmand/feeds/management/commands/update_feeds.py
+++ gourmand/feeds/management/commands/update_feeds.py
@@ -11,8 +11,9 @@ class Command(BaseCommand):
         self.verbosity = options.get('verbosity')
         to_update_qs = Feed.objects.filter(subscription__isnull=False)
         update_len = to_update_qs.count()
+        errors = 0
         for idx, feed in enumerate(to_update_qs, 1):
-            if self.verbosity > 1:
+            if self.verbosity > 2:
                 self.stdout.write('{} of {}: Updating {} at {}'.format(idx, update_len, feed.title, feed.href))
             try:
                 feed.update()
@@ -21,6 +22,9 @@ class Command(BaseCommand):
             except ValidationError as e:
                 if self.verbosity:
                     self.stderr.write('Error updating {}: {}'.format(feed.href, e))
+                errors += 1
             except Exception as e:
                 self.stderr.write('FATAL ERROR updating {}: {}'.format(feed.href, e))
                 raise e
+        if self.verbosity > 1:
+            self.stdout.write('Tried to update {} feeds, {} successful, {} errors'.format(update_len, update_len - errors, errors))
