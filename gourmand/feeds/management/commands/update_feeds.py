fef73050d2b28fd0290a81ca65d26ba2288ab845
Aaron Kurtz <a.kurtz@hardsun.net>
2015-12-09 06:30:35 -0800

    Clean up update_feeds command

--- gourmand/feeds/management/commands/update_feeds.py
+++ gourmand/feeds/management/commands/update_feeds.py
@@ -1,26 +1,26 @@
-import socket
-
 from django.core.management.base import BaseCommand
 from django.core.exceptions import ValidationError
 
 from feeds.models import Feed
-
-TIMEOUT = 120
-socket.setdefaulttimeout(TIMEOUT)
 
 
 class Command(BaseCommand):
     help = "Check all existing feeds with subscriptions for updates"
 
     def handle(self, *args, **options):
-        for feed in Feed.objects.filter(subscription__isnull=False):
+        self.verbosity = options.get('verbosity')
+        to_update_qs = Feed.objects.filter(subscription__isnull=False)
+        update_len = to_update_qs.count()
+        for idx, feed in enumerate(to_update_qs, 1):
+            if self.verbosity > 1:
+                print('{} of {}: Updating {} at {}'.format(idx, update_len, feed.title, feed.href))
             try:
                 feed.update()
                 for sub in feed.subscription_set.all():
                     sub.populate()
             except ValidationError as e:
-                print(feed.href)
-                print(e)
+                if self.verbosity:
+                    print('Error updating {}: {}'.format(feed.href, e))
             except Exception as e:
-                print(feed.href)
+                print('FATAL ERROR updating {}: {}'.format(feed.href, e))
                 raise e
