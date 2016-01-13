8806db846158d4ff31fa2562e05180eb46e41f3e
Aaron Kurtz <a.kurtz@hardsun.net>
2016-01-12 23:39:37 -0800

    Add str method to Category

--- gourmand/subscriptions/models.py
+++ gourmand/subscriptions/models.py
@@ -14,6 +14,9 @@ class Category(models.Model):
         ordering = ('order',)
         verbose_name_plural = 'categories'
 
+    def __str__(self):
+        return u"{} [{}]: {}".format(self.owner, self.order, self.name)
+
 
 class Subscription(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL)
