f5eb3dea5bac58b00b54d592f718178ad6fb851d
Aaron Kurtz <a.kurtz@hardsun.net>
2016-01-12 23:39:58 -0800

    Add custom query method for Categories
    Users should always have an 'Uncategorized' Category. Using a custom
    manager and the 'get_user_categories' method, we can create it if it
    doesn't currently exist.

--- gourmand/subscriptions/models.py
+++ gourmand/subscriptions/models.py
@@ -4,10 +4,22 @@ from django.conf import settings
 from feeds.models import Feed, Article
 
 
+class CategoryManager(models.Manager):
+    def get_user_categories(self, user):
+        categories = super().get_queryset().filter(owner=user)
+        if categories:
+            return categories
+        else:
+            Category.objects.create(owner=user, order=0, name="Uncategorized")
+            return super().get_queryset().filter(owner=user)
+
+
 class Category(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL)
     order = models.PositiveSmallIntegerField()
     name = models.TextField()
+
+    objects = CategoryManager()
 
     class Meta:
         unique_together = (('owner', 'name'), ('owner', 'order'))
