04fe1dc78fb712ac07d76067768c37293c50e6b1
Aaron Kurtz <a.kurtz@hardsun.net>
2016-01-15 01:23:22 -0800

    Prettify subscription extra argument

--- gourmand/subscriptions/views.py
+++ gourmand/subscriptions/views.py
@@ -39,9 +39,9 @@ class Reader(LoginRequiredMixin, TemplateView):
         subs = Subscription.objects.filter(owner=self.request.user).select_related('feed')
         # Extra modifier is required to annotate unread as well as articles
         # Conditional Count in 1.8 works, but breaks if combined with another Count, despite using distinct=True
-        subs = subs.annotate(articles=Count('feed__article')).\
-            extra(select={'unread': 'SELECT COUNT(*) FROM subscriptions_personalarticle WHERE ' +
-                          'subscriptions_subscription.id = subscriptions_personalarticle.sub_id AND active IS TRUE'})
+        subs = subs.annotate(articles=Count('feed__article')).extra(
+            select={'unread': 'SELECT COUNT(*) FROM subscriptions_personalarticle WHERE ' +
+                    'subscriptions_subscription.id = subscriptions_personalarticle.sub_id AND active IS TRUE'})
         subs.order_by('feed__title')
         context['subs'] = subs
         return context
