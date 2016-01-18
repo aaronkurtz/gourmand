b8503a57dd128a1dd27a26ddbe8b78f3808dfd0c
Aaron Kurtz <a.kurtz@hardsun.net>
2016-01-17 21:52:04 -0800

    Improve category admin

--- gourmand/subscriptions/admin.py
+++ gourmand/subscriptions/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.template.loader import render_to_string
+
 from .models import Subscription, PersonalArticle, Category
 
 
@@ -14,4 +16,15 @@ class PersonalArticleAdmin(admin.ModelAdmin):
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('owner', 'order', 'name')
+    list_display_links = ('name',)
+    readonly_fields = ('owner', 'show_subs')
+    fields = ('owner', ('order', 'name'), 'show_subs')
+    ordering = ('owner', '-order')
+    search_fields = ('owner__username',)
+
+    def show_subs(self, instance):
+        qs = instance.subs.select_related('feed').order_by('feed__title')
+        return render_to_string('fragments/show_subs.html', {'subs': qs})
+    show_subs.short_description = 'Subscriptions'
+    show_subs.allow_tags = True
