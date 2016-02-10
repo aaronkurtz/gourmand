f430346bf5da03658ed60d39851bb0574ad90c1a
Aaron Kurtz <a.kurtz@hardsun.net>
2016-02-10 13:16:05 -0800

    Move OPML export under /account/

--- gourmand/subscriptions/urls.py
+++ gourmand/subscriptions/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
     url(r'^reader/$', Reader.as_view(), name="reader"),
     url(r'^reader/add$', AddSubscription.as_view(), name="add_subscription"),
     url(r'^reader/opml/import$', ImportOPML.as_view(), name="import_opml"),
-    url(r'^reader/opml/export$', ExportOPML.as_view(), name="export_opml"),
     url(r'^reader/(?P<pk>[0-9]+)/$', PersonalArticleList.as_view(), name="subscription"),
     url(r'^reader/(?P<pk>[0-9]+)/edit$', UpdateSubscription.as_view(), name="update_subscription"),
     url(r'^reader/(?P<pk>[0-9]+)/mark_read$', MarkRead.as_view(), name="mark_read"),
@@ -18,4 +17,5 @@ urlpatterns = [
     url(r'^articles/(?P<pk>[0-9]+)/$', ArticleReader.as_view(), name="article"),
     url(r'^account/$', Account.as_view(), name="account"),
     url(r'^account/delete$', DeleteAccount.as_view(), name="remove_account"),
+    url(r'^account/opml/export$', ExportOPML.as_view(), name="export_opml"),
 ]
