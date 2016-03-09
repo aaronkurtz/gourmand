9d7b7e73afb26ba8048bf52bda7e97373d8067bd
Aaron Kurtz <a.kurtz@hardsun.net>
2016-03-08 16:50:43 -0800

    Add fix_content function

--- gourmand/subscriptions/utils.py
+++ gourmand/subscriptions/utils.py
@@ -1,7 +1,14 @@
 import io
+from urllib.parse import urlparse
 from xml.etree import cElementTree as ET
 
+from bs4 import BeautifulSoup
+
 from .models import Subscription
+
+SSLIFY_HOST_LIST = (
+    'youtube.com', 'www.youtube.com'
+)
 
 
 def create_opml(user):
@@ -17,3 +24,30 @@ def create_opml(user):
     et = ET.ElementTree(opml)
     et.write(f, encoding='utf-8', xml_declaration=True)
     return f.getvalue()
+
+
+def fix_content(content):
+    """
+    Parse article content to rewrite it for a better reader experience
+    """
+    parsed_content = BeautifulSoup(content)
+    for img in parsed_content.find_all('img'):
+        del img['srcset']
+        del img['sizes']
+        img['class'].append('img-responsive')
+
+    for div in parsed_content.find_all('div'):
+        del div['style']
+
+    for table in parsed_content.find_all('table'):
+        table['class'].append('table-responsive')
+
+    for a in parsed_content.find_all('a'):
+        a['target'] = '_blank'
+
+    for iframe in parsed_content.find_all('iframe'):
+        url = urlparse(iframe['src'])
+        if url.scheme != 'https' and url.netloc in SSLIFY_HOST_LIST:
+            iframe['src'] = url._replace(scheme='https').geturl()
+
+    return str(parsed_content)
