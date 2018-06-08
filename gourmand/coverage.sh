fbef8a9dc961ba7b0accde642e58e8b7409b421f
Aaron Kurtz <a.kurtz@hardsun.net>
2018-06-08 16:40:26 -0700

    Remove Opbeat

--- gourmand/coverage.sh
+++ gourmand/coverage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 python manage.py collectstatic --noinput
-OPBEAT_DISABLE_SEND=true coverage run --branch --source='.' manage.py test
+coverage run --branch --source='.' manage.py test
 coverage report -m --omit=*migrations*,*__init__.py,*tests*
